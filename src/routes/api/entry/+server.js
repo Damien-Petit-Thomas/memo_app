86ea69822043ac35cd96f67e74531f9e45839077
damien.petit.thomas <damien.petit.thomas@gmail.com>
2024-02-09 02:38:58 +0100

    typo

--- src/routes/api/entry/+server.js
+++ src/routes/api/entry/+server.js
@@ -1,18 +1,16 @@
 import { json } from '@sveltejs/kit';
 import db from '$lib/db';
-import CoreDatamapper from '$lib/model/datampper.js';
+import CoreDatamapper from '$lib/model/care.datamapper.js';
 
 const users = new CoreDatamapper(db, 'user');
 
 export async function POST({ request }) {
   const { formvalue } = await request.json();
-  console.log('formvalue', formvalue);
   const data = {
     name: formvalue.username,
     email: formvalue.email,
     password: formvalue.password,
   };
-  console.log('data', data)
   const result = await users.create(data);
   if (result) {
     return json(result);
